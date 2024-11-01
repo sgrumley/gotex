86121dbd8923a0a1a37006e38eecaf8aff90aef4
sgrumley <grumsamu@gmail.com>
2024-11-01 18:07:24 +1100

    minor fix

--- cmd/theme-tester/main.go
+++ cmd/theme-tester/main.go
@@ -65,6 +65,13 @@ import (
 )
 
 func main() {
+	// https://stackoverflow.com/questions/39627348/ansi-colours-on-windows-10-sort-of-not-working
+	// stdout := windows.Handle(os.Stdout.Fd())
+	// var originalMode uint32
+
+	// windows.GetConsoleMode(stdout, &originalMode)
+	// windows.SetConsoleMode(stdout, originalMode|windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING)
+
 	fmt.Println()
 	for _, flavour := range []catppuccin.Flavour{
 		catppuccin.Mocha,
